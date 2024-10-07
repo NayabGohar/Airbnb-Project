466532cca3bcac1b9a43a5f5f6fab2da90076704
NayabGohar <130220151+NayabGohar@users.noreply.github.com>
2024-10-07 23:46:29 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lightblue: '#add8e6', 
+        darkblue: '#00008B',
+      },
+    },
+  },
+  plugins: [],
+};
